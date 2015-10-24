edf91346599d4d4456e7e1e424e69908cc179e1b
RyodoTanaka <k104073r@mail.kyutech.jp>
2015-10-24 02:07:01 +0000

    [add] なんのパッケージを追加したかわすれたお

---
+++ srecode-map.el
@@ -0,0 +1,6 @@
+;; Object SRecode Map
+;; EIEIO PERSISTENT OBJECT
+(srecode-map "SRecode Map"
+  :file "srecode-map.el"
+  :files '(("/usr/local/share/emacs/24.4/etc/srecode/wisent.srt" . wisent-grammar-mode) ("/usr/local/share/emacs/24.4/etc/srecode/texi.srt" . texinfo-mode) ("/usr/local/share/emacs/24.4/etc/srecode/template.srt" . srecode-template-mode) ("/usr/local/share/emacs/24.4/etc/srecode/make.srt" . makefile-mode) ("/usr/local/share/emacs/24.4/etc/srecode/java.srt" . java-mode) ("/usr/local/share/emacs/24.4/etc/srecode/el.srt" . emacs-lisp-mode) ("/usr/local/share/emacs/24.4/etc/srecode/default.srt" . default) ("/usr/local/share/emacs/24.4/etc/srecode/cpp.srt" . c++-mode) ("/usr/local/share/emacs/24.4/etc/srecode/c.srt" . c-mode))
+  :apps '((tests ("/usr/local/share/emacs/24.4/etc/srecode/test.srt" . srecode-template-mode)) (getset ("/usr/local/share/emacs/24.4/etc/srecode/getset-cpp.srt" . c++-mode)) (ede ("/usr/local/share/emacs/24.4/etc/srecode/ede-make.srt" . makefile-mode) ("/usr/local/share/emacs/24.4/etc/srecode/ede-autoconf.srt" . autoconf-mode)) (document ("/usr/local/share/emacs/24.4/etc/srecode/doc-java.srt" . java-mode) ("/usr/local/share/emacs/24.4/etc/srecode/doc-default.srt" . default) ("/usr/local/share/emacs/24.4/etc/srecode/doc-cpp.srt" . c++-mode))))
