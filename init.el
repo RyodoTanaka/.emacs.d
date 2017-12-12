ea1a88f305d1354d90423f07bac6dcb296840b86
Ryodo Tanaka <groadpg@gmail.com>
2017-12-12 14:21:59 +0900

    [add] rust-mode

--- init.el
+++ init.el
@@ -338,8 +338,9 @@ user-mail-address "GRoadPG@gmail.com")
 
 ;;; MELPA
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;; 
