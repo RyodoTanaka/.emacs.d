60f0716361c25abd71ec5c7e0a573b0193d955f9
RyodoTanaka <groadpg@gmail.com>
2019-01-30 12:48:25 +0900

    [add] update packages

---
+++ elpa/dashboard-hackernews-20190109.205/dashboard-hackernews-autoloads.el
@@ -0,0 +1,23 @@
+;;; dashboard-hackernews-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "dashboard-hackernews" "dashboard-hackernews.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from dashboard-hackernews.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "dashboard-hackernews" '("dashboard-hackernews-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dashboard-hackernews-autoloads.el ends here
---
+++ elpa/dashboard-hackernews-20190109.205/dashboard-hackernews-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dashboard-hackernews" "20190109.205" "Display Hacker News on dashboard" '((emacs "24") (dashboard "1.2.5") (request "0.3.0")) :commit "b71814716d8f78181b9d1990f06072460de0797e" :authors '(("Hayato KAJIYAMA" . "kaji1216@gmail.com")) :maintainer '("Hayato KAJIYAMA" . "kaji1216@gmail.com") :url "https://github.com/hyakt/emacs-dashboard-hackernews")
