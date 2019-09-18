8a62383743eb9da302235c354711934aa764e2ae
RyodoTanaka <groadpg@gmail.com>
2019-09-19 02:40:36 +0900

    Update theme

--- init.el
+++ init.el
@@ -1,3 +1,29 @@
+;;; init.el ---                    
+;;; Copyright (C) 2019  Ryodo Tanaka
+;;; Author: Ryodo Tanaka
+;;; Code:
+
+;;;;;;;;;;;;;;;;;;
+;; Requirements ;;
+;;;;;;;;;;;;;;;;;;
+(prog1 "Change user-emacs-directory"
+  ;; enable debug
+  (setq debug-on-error  t
+        init-file-debug t)
+
+  ;; you can run like 'emacs -q -l ~/hoge/init.el'
+  (when load-file-name
+    (setq user-emacs-directory
+          (expand-file-name (file-name-directory load-file-name))))
+
+  ;; change user-emacs-directory
+  (setq user-emacs-directory
+        (expand-file-name
+         (format "local/%s.%s/"
+                 emacs-major-version emacs-minor-version)
+         user-emacs-directory))
+  (make-directory user-emacs-directory t))
+
 (prog1 "prepare leaf"
   (prog1 "package"
     (custom-set-variables
@@ -25,21 +51,37 @@
     (leaf hydra :ensure t)
     (leaf el-get :ensure t
       :custom ((el-get-git-shallow-clone  . t)))))
+
+;;;;;;;;;;;;;;;;;;;;;
+;; Original Custom ;;
+;;;;;;;;;;;;;;;;;;;;;
+;; Theme
+(leaf gruvbox-theme
+  :when (version<= "24.1" emacs-version)
+  :ensure t
+  :config
+  (load-theme 'gruvbox-dark-hard t))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; custom-set settings ;;                        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This part will work with GUI setting on Emacs ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(el-get-git-shallow-clone t)
- '(package-archives
-   (quote
-    (("org" . "https://orgmode.org/elpa/")
-     ("melpa" . "https://melpa.org/packages/")
-     ("gnu" . "https://elpa.gnu.org/packages/"))))
- '(package-selected-packages (quote (el-get hydra leaf-keywords leaf))))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(provide 'init)
+;;; End:
+;;; init.el ends here
